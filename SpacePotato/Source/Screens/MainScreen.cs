a36bbcecbc8f2b7cf936f9b79573a724e646e1ea
Peter Downey <peter.d.downey@gmail.com>
2021-05-29 22:46:42 -0400

    player renders after world

--- SpacePotato/Source/Screens/MainScreen.cs
+++ SpacePotato/Source/Screens/MainScreen.cs
@@ -82,10 +82,10 @@ namespace SpacePotato {
 
             // rendering code
             particlesUnder.Render(spriteBatch);
-            
-            _player.Render(spriteBatch);
 
             LevelManager.level.Render(gameTime, spriteBatch);
+
+            _player.Render(spriteBatch);
 
             particlesOver.Render(spriteBatch);
             // end
