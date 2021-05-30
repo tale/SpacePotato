cae276b5ac85340565f83ddc93fcd627ab570e06
Peter Downey <peter.d.downey@gmail.com>
2021-05-30 05:36:26 -0400

    levels 1-5

--- SpacePotato/Source/Util/LevelManager.cs
+++ SpacePotato/Source/Util/LevelManager.cs
@@ -36,6 +36,9 @@ namespace SpacePotato {
 
         public static void LoadLevels() {
             string[] files = Directory.GetFiles(Paths.dataPath, "*.xml");
+
+            files = files.AsEnumerable().OrderBy(f => f).ToArray();
+            
             _levels = new Level[files.Length + 1];
 
             // random debug level
@@ -59,14 +62,17 @@ namespace SpacePotato {
             
             
             _levels[0] = new Level(level1Planets, new Rectangle(-100, -1000, 6000, 2000), 1);
-
+            
             // file loaded levels
             for (int i = 1; i <= files.Length; i++) {
                 _levels[i] = DataSerializer.Deserialize<Level>(files[i-1]);
                 _levels[i].setUpSerialized();
+                
+                Console.WriteLine(files[i - 1]);
             }
 
             level = _levels[CurrentLevel];
+            
         }
     }
 }
